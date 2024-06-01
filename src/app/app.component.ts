b0611120d4de145c33fb790238e4f4a19e3a9ff8
dahyanne <dahyannealvarez@gmail.com>
2024-06-01 18:43:09 -0500

    commit repo angular capicua

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, FormsModule],
+  template: `
+    <div>
+      <h1>Analizador de Números Capicúa</h1>
+      <input type="text" [(ngModel)]="numero" (input)="analizarNumero()" placeholder="Ingresa un número" />
+      <p *ngIf="mensaje">{{ mensaje }}</p>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'Angular1';
+  numero: string = '';
+  esCapicua: boolean = false;
+  mensaje: string = '';
+
+  analizarNumero() {
+    if (this.numero.length >= 2) {
+      this.esCapicua = this.numero === this.numero.split('').reverse().join('');
+      this.mensaje = this.esCapicua ? 'El número es capicúa' : 'El número no es capicúa';
+    } else {
+      this.mensaje = '';
+      this.esCapicua = false;
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: '',
+   component: AppComponent,
+}
+];
